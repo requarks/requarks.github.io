7c10c7c6c95584b71a873c517d00a075f0901355
Nicolas Giard <github@ngpixel.com>
2017-03-24 18:03:20 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+if (window.location.protocol !== 'https:') {
+	window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
 (function($) {
 
 	skel.breakpoints({
